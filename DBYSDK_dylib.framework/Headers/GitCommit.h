eec90b933d1ac5d0464d28bb63b3c0913b23901b
zhongfan <fan.zhong@duobei.com>
2020-04-16 10:15:35 +0800

    update:更新sdk

--- DBYSDK_dylib.framework/Headers/GitCommit.h
+++ DBYSDK_dylib.framework/Headers/GitCommit.h
@@ -6,9 +6,9 @@
 *
 ********************************************************/
 #pragma once
-#define GIT_VERSION     "v2.3.1.3_saas-2204-ga7c5b2"
-#define GIT_DATE     "2020-04-14"
-#define GIT_HASH     "a7c5b2200f84462d24240190766c4cd01a84dc56"
+#define GIT_VERSION     "v2.3.1.3_saas-2208-g1f3bb8"
+#define GIT_DATE     "2020-04-15"
+#define GIT_HASH     "1f3bb8138facd5cf789afc65784279759e65058a"
 #define GIT_BRANCH     "develop"
 #define GIT_USER_NAME     "guochao"
 #define GIT_USER_EMAIL     "1303993457@qq.com"
